d94f5c87ca0c9445a37c71b77c39790e025b7893
MOHD NAZIR ANWER <122814116+naziranwer@users.noreply.github.com>
2024-01-06 00:51:31 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+// Importing products from products.json file
+const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/product.json`)
+);
+
+// Middlewares
+app.use(express.json())
+
+// Write POST endpoint for creating new product here
+// Endpoint /api/v1/products
+
+// GET endpoint for sending the details of users
+app.get('/api/v1/products', (req,res) => {
+    res.status(200).json({
+    status:'Success',
+    message:'Details of products fetched successfully',
+    data:{
+        products
+    }
+});
+});
+app.get('/api/v1/products/:id', (req,res) => {
+    let {id} = req.params;
+    id *=1;
+
+    const product = products.find(product => product.id===id);
+    if(!product){
+        return res.status(404).send({status:"failed", message: "Product not found!"});
+    }
+ 
+    res.status(200).send({
+        status : 'success',
+        message : "Product fetched successfully",
+        data: {
+            product
+        }
+});
+});
+    
+module.exports = app;
+

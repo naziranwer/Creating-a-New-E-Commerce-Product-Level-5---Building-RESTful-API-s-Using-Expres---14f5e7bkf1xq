3916892d14d1ddfbbe7199133f70bbd92d290645
Nazir <anwernazir674@gmail.com>
2024-01-06 01:10:42 +0530

    inititial

--- src/app.js
+++ src/app.js
@@ -12,6 +12,33 @@ app.use(express.json())
 
 // Write POST endpoint for creating new product here
 // Endpoint /api/v1/products
+app.post("/api/v1/products",(req,res)=>{
+    const {name,price,quantity}=req.body;
+
+    if(!name || !price || !quantity || isNaN(price) || isNaN(quantity)){
+        res.status(400).json({ status: "Error", message: "Invalid product data" });
+        return;
+    }
+
+    const newId= products.length === 0 ? 1 : products[products.length-1].id+1;
+
+    const newProduct={
+        id:newId,
+        name:name,
+        price:price,
+        quantity:quantity
+    };
+
+    products.push(newProduct);
+
+    fs.writeFileSync(`${__dirname}/data/products.json`,JSON.stringify(products,null,2));
+
+    res.status(201).json({
+        status: "Success",
+        message: "Product added successfully",
+        data: { newProduct }
+    });
+})
 
 // GET endpoint for sending the details of users
 app.get('/api/v1/products', (req,res) => {
